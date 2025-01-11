3f545448b0228aafc4825e2d9e530dec277118c9
Tarun Sirohi <tarunsirohi22@gmail.com>
2025-01-11 22:09:47 +0530

    Add File

---
+++ ConstructKPalindromeStrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k) return false;
+
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c,0)+1);
+        }
+
+        int oddCount=0;
+        for (int count : freq.values()){
+            if( count % 2 != 0) {
+                oddCount++;
+            }
+        }
+        return (oddCount <=k);
+    }
+}
